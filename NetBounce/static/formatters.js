0a181a329cd49cbf9c12c139104dbecc3f3745e7
Earlz <earlz@lastyearswishes.com>
2013-06-29 10:35:08 -0400

    Added JSON formatter Adding some styling so it's not so ugly and improving content of landing page

--- NetBounce/static/formatters.js
+++ NetBounce/static/formatters.js
@@ -1,13 +1,17 @@
 formatters = [];
 formatters.push({name: 'None', func: function(data, unescaped){return data;}});
 formatters.push({name: 'XML formatter', func: xmlFormatter});
+formatters.push({name: 'JSON formatter', func: jsonFormatter});
 
 function xmlFormatter(data, unescaped){
 	src=vkbeautify.xml(unescaped).escape();
 	var expr = new RegExp("\n","g")
     data=src.replace(expr,'<br />');
-	return '<pre onload="prettyPrint()" class="prettyprint" id="xml">'+data+'</pre>';
+	return '<pre onload="prettyPrint()" class="prettyprint">'+data+'</pre>';
 }
-function jsonFormatter(data){
-	return '<pre onload="prettyPrint()" class="prettyprint" id="json">'+data+'</pre>';
+function jsonFormatter(data, unescaped){
+	src=vkbeautify.json(unescaped).escape();
+	var expr = new RegExp("\n","g")
+    data=src.replace(expr,'<br />');
+	return '<pre onload="prettyPrint()" class="prettyprint">'+data+'</pre>';
 }
